36bf9cc024f43c2ebd50829b66a90fc727161740
ByWizKi <enzoth39260@gmail.com>
2023-05-17 11:12:36 +0200

    TP1 Terminer 100% (revision)

--- TP1/TP1.ml
+++ TP1/TP1.ml
@@ -1,81 +1,107 @@
 (*Premiers pas avec l'interperteur*)
 
 (*Definition de fonction*)
-(* fonction qui ajoute 1 *)
-let f (x:int) :int = x+1;;
 
-(*fonction qui calcule le discriminant*)
-let discriminant (a:int)  (b:int)  (c:int) :float = 
-  float_of_int(b*b-(4*a*c));;
+(**
+    [caculeDiscriminant ax bx c] calcule le discriminant d'un polynome du second degres.
+    Cette fonction est utile pour trouve les racines du polynomes ax^2 + bx + c
+    @param ax le coef d'ordre 2
+    @param bx le coef d'ordre 1
+    @param c le coef d'ordre 0
+    @return le discriminant
+*)
+let calculeDiscriminant (ax : float) (bx :float) (c: float) : float = (bx**2.) -. (4.*.ax*.c);;
 
-(*Type somme*)
 
-type couleur = Rouge
-              | Jaune
-              | Bleu
+(*Type Somme*)
+type couleur = Rouge 
+              | Jaune 
+              | Bleu 
               | Violet
               | Orange
               | Vert
-              | RJB of int * int * int ;;
+              | RJB of int*int*int;;
+ 
+type mes_valeurs = UnInt of int
+                  | DeuxInt of int*int
+                  | UnString of string;;
+let nom_couleur (couleur : couleur) : string =
+  match couleur with
+  Bleu -> "Bleu"
+  |Rouge -> "Rouge"
+  |Jaune -> "Jaune"
+  |Vert-> "Vert"
+  |Orange -> "Orange"
+  |_ -> "melange de couleur";;
 
-type mes_valeur = UnInt of int
-                  | DeuxInt of int * int
-                  | UneChaine of string;;
+(**
+[paragraphe_bottles est une fonction qui prend en argument un nombre de bouteille et permet de gere l'orthographe des mots pour la chason beer]
+@param nombre_bouteille un entier
+@return le paragraphe de la chanson avec la bonne orthographe    
+*)
+let paragraphe_bottles (nombre_bouteille : int) : string =
+  match nombre_bouteille with 
+  0 ->  "No more bottles of beer on the well, no more bottles of bear\n Go to the store and buy some more, 99 bottles of beer on the wall."
+  | 1 -> "1 bottle of beer on the wall, 1 bottle of beer.\n Take one down and pass it around no more bottles of beer on the wall."
+  | 2  -> "2 bottles of beer on the wall, 2 bottles of beer. \n Take one doewn and pass it around, 1 bottle of beer in the wall."
+  | _ -> string_of_int(nombre_bouteille)^" bottles of beer on the wall, "^string_of_int(nombre_bouteille)^" bottles of beer.\ntake one down and pass it around, "^
+  string_of_int(nombre_bouteille-1)^" bottles of beer in the wall.";;
 
-(*Pattern matching*)
+(*Recursivité*)
 
-let nom_couleur (c:couleur) : string =
-  match c with
-  Bleu -> "bleu"
-  | Rouge -> "rouge" 
-  | Jaune -> "jaune"
-  | Violet -> "violet"
-  | Orange -> "orange"
-  | Vert -> "vert"
-  | RJB(_,_,_) -> "melange";;
+(**
+[factorielle] renvoie la factorielle n
+@param n un entier 
+@return la factiorielle du paramètre
+*)
 
+let rec factiorielle (n:int) : int =
+  if(n = 0 || n = 1)
+    then 1
+else n*factiorielle(n-1);;
 
-let niveau_rouge c =
-  match c with
-  | Rouge -> 255
-  | Orange -> 255
-  | Violet -> 255
-  | RJB (r,j,b) -> r
-  | _ ->  0;;
+(*Listes*)
 
-let paragraphe_bottles (nb:int) : string = 
-  match nb with
-  0 -> "No more bottles of beer on the wall, no more bottles of beer.
-  Go to the store and buy some more, 99 bottles of beer on the wall."
-  | 1 -> "1 bottle of beer on the wall, 1 bottle of beer. Take one down and pass it around, no more bottles of beer on the wall."
-  | 2 -> "2 bottles of beer on the wall, 2 bottles of beer. Take one down and pass it around, 1 bottle of beer on the wall."
-  | x -> string_of_int x ^ " bottles of beer on the wall, " ^ string_of_int x ^ " bottles of beer. Take one down and pass it around, " ^ string_of_int (x-1) ^ " bottles of beer on the wall.";;
+(**
+  [sum_f] est un fonction qui permet de faire la sommes des floats d'une liste
+  @param l une liste de floats
+  @return un floats qui est la somme des floats de la liste. 
+*)
 
-(*Recursivite*)
-
-let rec factorielle (n:int) : int =
-  match n with 
-  0 -> 1
-  | 1 -> 1
-  | _ -> n * factorielle(n-1);;
-
-(*Liste*)
-
-let rec join_s (s:string) (l: string list) : string =
+let rec sum_f (l : float list) : float = 
   match l with
-   [] -> ""
-  | x::r -> x ^ s ^ join_s s r;; 
+  [] -> 0.
+  | x::r -> x+.sum_f(r);;
 
-let rec liste_n_0 (n:int) : int list =
-  match n with
-  0 -> [0]
-  | _ -> n::liste_n_0 (n-1);;
+(**
+    [join_s] cette fonction permet a partir d'une liste de string et d'un separateur de type string de construire un string
+    @param une liste de string
+    @param un serapateur de type string
+*)
+let rec join_s (l : string list) (s : string) = 
+  match l with
+  [] -> ""
+  | [x] -> x^(join_s [] s)
+  | x::r -> x^s^(join_s r s);; 
 
-let rec bottles_of_list (l:int list) = 
-  match l with 
-  [] -> failwith("Aucun int dans la liste")
-  | [x] -> [paragraphe_bottles x]
-  | x::r -> paragraphe_bottles x :: bottles_of_list r;;
+(**
+    [liste_n_0] cette fonction permet de fabriquer une liste de n à 0 
+    @param n un entier
+    @return un liste d'entier dans l'ordre décroissant (n à 0)
+*)
+let rec liste_n_0 (n:int) : int list = 
+  if (n >= 0 )
+    then n::(liste_n_0 (n-1))
+else [];;
 
-(* let chanson_99_bottles = join_s "\n" ((bottles_of_list) (liste_n_0 99));;
-print_endline chanson_99_bottles;; *)
+(**
+    [bottles_of_liste] cette fonction permet de renvoyer les n paragraphes de la chanson 99 bottles
+    @param n un entier qui est le nombre de paragrphe voulue
+    @return une liste de string ou les string sont des paragraphes
+*)
+let rec bottles_of_liste (n : int) : string list= 
+  if (n >=0)
+    then (paragraphe_bottles n)::bottles_of_liste(n-1)
+else [];;
+
+let chanson_99_bottles = join_s (bottles_of_liste 99) "\n\n";;
