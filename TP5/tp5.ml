47359f433f8d0d3b30418f55c142b830ada4e4fc
ByWizKi <enzoth39260@gmail.com>
2023-05-19 11:43:29 +0200

    Exo 1 terminer

--- TP5/tp5.ml
+++ TP5/tp5.ml
@@ -1,46 +1,70 @@
 
 (* Partie 1 Arbre n-aires :recodage *)
 
-type 'a arbre_n = Feuille of 'a | Noeud of 'a arbre_n list
+type 'a arbre_n = Feuille of 'a | Noeud of 'a arbre_n list;;
 
-let a1 = Feuille 1
-let a2 = Feuille 2
-let a3 = Noeud []
-let a4 = Noeud [ a1 ]
-let a5 = Noeud [ a1; a2 ]
-let a6 = Noeud [ a1; a2; a3; a4; a5 ]
+let arbre1 = Noeud[Feuille 4; Noeud[Feuille 0; Feuille 1]; Noeud[Feuille 0]];;
 
-let rec hauteur (l : 'a arbre_n) : int =
-  match l with 
-  Feuille _ -> 1;
-  | Noeud l ->  1 + List.fold_left max 0 (List.map hauteur l) ;;
+(**
+    [hauteur_arbre] cette fonction permet de calculer la hauteur d'un arbre avec list.map
+    @param un arbre_n
+    @return la hauteur d'un arbre
+*)
+let rec hauteur_arbre (a : 'a arbre_n) : int = 
+  match a with
+  | Feuille x -> 1 
+  | Noeud(f) -> 1 + List.fold_left max  0 (List.map hauteur_arbre f);;
 
-assert (hauteur a1 = 1);;
-assert (hauteur a3 = 1);;
-assert (hauteur a4 = 2);;
-assert (hauteur a5 = 2);;
+assert(hauteur_arbre arbre1 = 3);;
 
+(**
+    [list_of_arbre] cette fonction permet a partir d'un arbre mettre tous les elements dans une liste
+    @param un arbre_n
+    @return une liste qui contine tout les elements d'un arbre
+*)
 
-let list_of_arbre (a : 'a arbre_n) : 'a list =
-  let rec list_of_arbre_aux (a: 'a arbre_n) (acc : 'a list) : 'a list =
-    match a with
-    Feuille x -> x::acc
-    | Noeud l -> List.fold_right list_of_arbre_aux l acc in 
+let list_of_arbre (a : 'a arbre_n) : 'a list = 
+  let rec list_of_arbre_aux (a : 'a arbre_n) (acc : 'a list) = 
+  match a with 
+  Feuille x -> x::acc
+  | Noeud(f) -> List.fold_right (list_of_arbre_aux) f acc in 
   list_of_arbre_aux a [];;
-
-assert (list_of_arbre a1 = [ 1 ]);;
-assert (list_of_arbre a4 = [ 1 ]);;
-assert (list_of_arbre a5 = [ 1; 2 ]);;
-assert (list_of_arbre a6 = [ 1; 2; 1; 1; 2 ])
-
-(* 1.2 Gestion d'option, fold et minimum *)
-
-let lift_option2 (f : 'a -> 'a -> 'a) : 'a option -> 'a -> 'a option = 
-  fun x y -> match x with None -> Some y | Some(z) -> Some(f z y);;
-
-
-let rec fold_left_arbre (f : 'a -> 'b arbre_n -> 'b arbre_n) (acc : 'b arbre_n) (arbre : 'a arbre_n) : 'b arbre_n = 
-  match arbre with
-  Feuille x -> f x acc 
-  | Noeud l -> 
   
+assert (list_of_arbre arbre1 = [4;0;1;0]);;
+
+(**
+    [lift_option_2] ce decorateur permet de d'appliquer une fonction binaire a 2 argument en parametre et de renvoyer le resultat sous type option
+    @param une fonction binaires
+    @param un resultat et type option
+    @param 'a 
+    @return 'a option
+*)
+let lift_option_2 (f : 'a -> 'a -> 'a) : 'a option -> 'a -> 'a option =
+    fun x y -> match x with None -> Some y | Some x' -> Some(f y x');;
+
+assert(lift_option_2 min (Some 3) 4  = Some(3));;
+
+(**
+    [fold_left_arbre] cette fonction peremet de parcourir un arbre et de lui appliquer une fonction a chacun de ses elements
+    @param une fonction qui modifie les elements
+    @param un acc
+    @param 'a arbre_n
+    @return le resultat de la fonction
+*)
+
+let rec fold_left_arbre (f : 'b -> 'a -> 'b) (acc : 'b) (a : 'a arbre_n) : 'b = 
+  match a with 
+  Feuille x -> f acc x
+  | Noeud n -> List.fold_left (fold_left_arbre f) acc n;;
+
+(**
+    [reduce] cette fonction permet de combiner 2 resultat de maniere generaliser avec la fonction fold_left_arbre
+    @param f une fonction
+    @param un 'a arbre_n
+*)
+
+let reduce (f : 'b -> 'a -> 'b) (a : 'a arbre_n) : 'b option = 
+  (fold_left_arbre (lift_option_2 f) None a);;
+
+assert(reduce (min) arbre1 = Some 0);;
+
